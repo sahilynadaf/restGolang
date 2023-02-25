e4e2f22163a62b6c42835b59a7b1264a2e9d4e41
Sahil Nadaf <sahilynadaf@gmail.com>
2023-02-26 01:15:46 +0530

    add todo structure

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type todo struct {
+	ID        string
+	Item      string
+	Completed bool
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record Video", Completed: false},
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
